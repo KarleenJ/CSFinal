1edb19a886d7d30f9fb7d2dc543f680c9ed04a3d
KarleenJ <56487033+KarleenJ@users.noreply.github.com>
2020-05-31 23:16:28 -0400

    Add files via upload

---
+++ PetStore/Models/Owner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Models
+{
+    public class Owner
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => this.FirstName + " " + this.LastName;
+        
+        [Required]
+        [MaxLength(20)]
+        public string Street { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string City { get; set; }
+
+        [Required]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
+        public string EmailAddress { get; set; }
+        
+        [Required]
+        [RegularExpression(@"^[0-9]{10}$)|(^\+[0-9]{2}\s+[0-9]{2}[0-9]{8}$)|(^[0-9]{3}-[0-9]{4}-[0-9]{4}$")]
+        public string PhoneNumber { get; set; }
+       
+        [Required]
+        [MaxLength(20)]
+        public string Occupation { get; set; }
+
+        public ICollection<Pet> Pets { get; set; }
+
+
+    }
+}
---
+++ PetStore/Models/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Name { get; set; }
+        
+        [Required]
+        public DateTime DOB { get; set; }
+       
+        public enum Breed { Dog, Cat, Fish, Hamster, GuineaPig }
+        
+        [Required]
+        public int OwnderId { get; set; }
+
+        public Owner Owner { get; set; }
+
+
+    }
+}
